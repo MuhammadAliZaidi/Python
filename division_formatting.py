e7164ab6bfaeb670443176b843615480c5687d8b
MuhammadAliZaidi <aghazaidi@yandex.com>
2026-03-15 22:00:45 +0500

    Format division output with f-string
    Format division result to two decimal places using f-string.

---
+++ division_formatting.py
@@ -0,0 +1,7 @@
+x=float(input("Enter first digit: "))
+y=float(input("Enter second digit: "))
+#z=round(x/y,5)
+#print(z)
+# to do the same wuth f string we will do the following
+z= (x/y)
+print(f"{z:.2f}")
