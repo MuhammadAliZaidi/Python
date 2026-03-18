6092953045884668336c7f92219d5c05f88f0c3d
MuhammadAliZaidi <aghazaidi@yandex.com>
2026-03-18 17:04:10 +0500

    Improve is_even function implementation
    Refactor is_even function to use a more Pythonic approach.

--- parity.py
+++ parity.py
@@ -1,13 +1,21 @@
 def main():
-    x=int(input("Enter the value of x : "))
+    x = int(input("Enter the value of x : "))
     if is_even(x):
         print('Even')
     else:
         print('Odd')
+
 def is_even(n):
-   # if n%2==0:
-    #    return True
+    # Method 1: Full if-else (basic and very clear)
+    # if n % 2 == 0:
+    #     return True
     # else:
-      #  return False
-    return True if n%2==0 else False
+    #     return False
+
+    # Method 2: Short (ternary operator)
+    # return True if n % 2 == 0 else False
+
+    # Method 3: Most succinct (Pythonic way)
+    return n % 2 == 0
+
 main()
